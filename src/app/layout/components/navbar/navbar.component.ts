62ce282754c17f68507f42120e93d33af5df312f
Daniel de Brito <danieldebrito@outlook.com>
2021-03-27 16:00:27 -0300

    login google

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  public user$: Observable<any> = this.authSvc.afAuth.user;
 
-  ngOnInit(): void {
+  constructor( public authSvc: AuthService, private router: Router) { }
+
+  async onLogout() {
+    try{
+      await this.authSvc.logout();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public console(user){
+    console.log(user);
   }
 
 }
